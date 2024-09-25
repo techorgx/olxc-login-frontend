6b79c58e409ed6376e04ea6a252780e42767278b
Aman Rajput <amrajput@expediagroup.com>
2024-09-25 20:24:23 +0530

    Combined username and password component file

--- olxc-login-frontend/src/login/LoginForm.jsx
+++ olxc-login-frontend/src/login/LoginForm.jsx
@@ -1,13 +1,11 @@
-import Password from "./Password";
-import Username from "./Username";
+import LoginInput from "./LoginInput";
 import "./css/LoginForm.css"
 
 function LoginForm() {
     return (
         <div className="login-form-container">
             <form className="login-form">
-                 <Username></Username>
-                 <Password></Password>
+                 <LoginInput></LoginInput>
             </form>
         </div>
     );
