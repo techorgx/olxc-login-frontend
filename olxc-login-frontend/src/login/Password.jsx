f2560e88bd95c497e7e67df7ea71292c630650fa
Aman Rajput <amrajput@expediagroup.com>
2024-09-25 17:40:27 +0530

    Added a test API

--- olxc-login-frontend/src/login/Password.jsx
+++ olxc-login-frontend/src/login/Password.jsx
@@ -1,6 +1,30 @@
+import React from 'react';
 import './css/Password.css';
 
 function Password() {
+    const handleLoginClick = () => {
+      const apiUrl = 'http://localhost:8080/test';
+
+      fetch(apiUrl, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      })
+      .then((response) => {
+          if (!response.ok) {
+              throw new Error('Login failed');
+          }
+          return response.json();
+      })
+      .then((data) => {
+          console.log('Login successful', data);
+      })
+      .catch((error) => {
+          console.error('Error during login:', error);
+      });
+    };
+
     return (
       <>
       <input
@@ -8,9 +32,11 @@ function Password() {
         className="login-input-password"
         placeholder="Password"
        />
-       <button type="submit" className="login-button">Login</button>
+       <button type="button" className="login-button" onClick={handleLoginClick}>
+           Login
+       </button>
        </>
-      );
+    );
 }
 
-export default Password
+export default Password;
